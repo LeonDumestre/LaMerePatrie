041ac1654ed4842cd6183f1e905326ec455b1c01
Leon Dumestre <leon.dumestre@gmail.com>
2023-09-22 13:44:34 +0200

    add tscord template

--- Events/ready.js
+++
@@ -1,7 +0,0 @@
-const loadSlashCommands = require("../Loaders/loadSlashCommands");
-
-module.exports = async (bot) => {
-    
-	await loadSlashCommands(bot);
-	console.log(`\x1b[40m\x1b[33m${bot.user.tag} est arrivé !\x1b[0m`);
-};
